4f894711250ee0ed2e37c4de266fe66bf6a3c8f8
teste-agua-edu <gui.2001@hotmail.com>
2023-08-21 07:17:51 -0300

    vite config removed

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://apiadvisor.climatempo.com.br/",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 });
+
+
+// server: {
+//   proxy: {
+//     "/api": {
+//       target: "http://apiadvisor.climatempo.com.br/",
+//       changeOrigin: true,
+//       secure: false,
+//     },
+//   },
+// },
